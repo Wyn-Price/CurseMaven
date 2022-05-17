90da60b1ac140d0616ea2a9469d618faad0c6875
Wyn Price <wynprice999@gmail.com>
2022-05-17 18:50:07 +0100

    Clean up verify params, and add gradle version metric

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,13 @@
-import escapeHTML from "escape-html";
-import express, { RequestHandler } from "express";
-import createClassifierMap from "./classifiermap";
+import express from "express";
 import direct from "./direct";
 import normaljar from "./normaljar";
 import pom from "./pom";
 import testing from "./testing";
-import { log } from "./util";
+import verifyParams from "./verifyparams";
 
 const app = express();
 
 const urlBase = "/curse/maven/:descriptor/:fileIds/:filename"
-
-const verifyParams: RequestHandler = (req, res, next) => {
-  const { descriptor, fileIds, filename } = req.params
-  //Examples with curse.maven:example-jei-238222:2724420-sources-2724421:classifier -> /curse/maven/example-jei-238222/2724420/example-jei-238222-2724420-classifier.jar
-  //    descriptor: 'example-jei-238222'
-  //    fileIds: '2724420-sources-2724421'
-  //    filename: 'example-jei-238222-2724420-sources-2724421-classifier'
-
-  //['2724420', 'sources', '2724421']
-  const fileIdsSplit = fileIds.split("-")
-
-  //['example', 'jei', '238222']
-  const descriptorSplit = descriptor.split("-")
-  if (descriptorSplit.length < 2) {
-    return res.status(400).send("Descriptor now must be in the format `<modname>-<projectid>")
-  }
-
-  const map = createClassifierMap(fileIds);
-  if (map === null) {
-    return res.status(400).send("Unable to generate classifier map")
-  }
-  const { main, classifierMap } = map
-
-  const name = descriptorSplit.slice(0, descriptorSplit.length - 1).join("-")
-  const id = descriptorSplit[descriptorSplit.length - 1]
-  //From Example
-  //    name: 'example-jei'
-  //    id: '238222'
-
-  //['example', 'jei', '238222', '2724420', 'classifier']
-  const filenameSplit = filename.split("-")
-  if (filenameSplit.length < 3) {
-    return res.sendStatus(401)
-  }
-
-  //My brother in christ please rewrite this code. 
-  //Essentially, I'm using the previous URL artifacts to validate the URL, and get the classifier
-  const _name = filenameSplit.slice(0, descriptorSplit.length - 1).join("-")
-  const _id = filenameSplit[descriptorSplit.length - 1]
-  const _file = filenameSplit.slice(descriptorSplit.length).slice(0, fileIdsSplit.length).join("-")
-  const classifier = filenameSplit.slice(descriptorSplit.length + fileIdsSplit.length).join("-")
-  //From Example
-  //    _name: 'example-jei'
-  //    _id: '238222'
-  //    _file: '2724420'
-  //    classifier: 'classifier'
-
-  //Check the url parts match up
-
-  if (name !== _name || id !== _id || fileIds !== _file) {
-    return res.sendStatus(404)
-  }
-
-  const foundId = classifier === "" ? main : classifierMap[classifier]
-  if (foundId === undefined) {
-    return res.status(404).send(`Unable to find classifier ${escapeHTML(classifier)} as it was not defined.`)
-  }
-
-  res.locals.id = id
-  res.locals.file = foundId
-  res.locals.name = name
-
-  log(`project_id=${id},project_named=${name},file_id=${main},classifier=${classifier ?? 'n/a'}`)
-
-  next()
-}
 
 app.get(`${urlBase}.jar`, verifyParams, normaljar)
 app.get(`${urlBase}.pom`, verifyParams, pom)
---
+++ src/verifyparams.ts
@@ -0,0 +1,163 @@
+import escapeHTML from 'escape-html';
+import { Request, RequestHandler } from 'express';
+import createClassifierMap from './classifiermap';
+import { log } from './util';
+
+type SplitData = {
+  fileIdsSplit: string[];
+  descriptorSplit: string[];
+  filenameSplit: string[];
+}
+type DescriptorParts = {
+  name: string,
+  id: string
+}
+type FilenameParts = {
+  name: string,
+  id: string,
+  file: string,
+  classifier: string
+}
+
+const verifyParams: RequestHandler = (req, res, next) => {
+  try {
+    verifyParamsOrThrow(req, res, next)
+  } catch (err) {
+    if (err instanceof InvalidParamsError) {
+      return res.status(err.statusCode).send(err.text)
+    } else {
+      throw err
+    }
+  }
+}
+
+const verifyParamsOrThrow: RequestHandler = (req, res, next) => {
+  const gradleVersion = checkGradleVersion(req)
+
+  const { descriptor, fileIds, filename } = req.params
+  //Examples with curse.maven:example-jei-238222:2724420-sources-2724421:classifier -> /curse/maven/example-jei-238222/2724420/example-jei-238222-2724420-classifier.jar
+  //    descriptor: 'example-jei-238222'
+  //    fileIds: '2724420-sources-2724421'
+  //    filename: 'example-jei-238222-2724420-sources-2724421-classifier'
+
+
+  const splitData = splitAndValidate(fileIds, descriptor, filename)
+
+  const map = createClassifierMap(fileIds);
+  if (map === null) {
+    throw new InvalidParamsError(400, "Unable to generate classifier map")
+  }
+  const { main, classifierMap } = map
+
+  const descriptorParts = getNameAndIdFromDescriptor(splitData)
+  const filenameParts = createFilenameParts(splitData)
+
+  validatePartsEqual(descriptorParts, filenameParts, fileIds)
+
+  const classifier = filenameParts.classifier
+
+  const foundId = classifier === "" ? main : classifierMap[classifier]
+  if (foundId === undefined) {
+    return res.status(404).send(`Unable to find classifier ${escapeHTML(classifier)} as it was not defined.`)
+  }
+
+
+  res.locals.id = descriptorParts.id
+  res.locals.name = descriptorParts.name
+  res.locals.file = foundId
+
+  log(`project_id=${descriptorParts.id},project_named=${descriptorParts.name},file_id=${main},classifier=${classifier},gradle_version=${gradleVersion}`)
+
+  next()
+}
+
+const checkGradleVersion = (req: Request) => {
+  const ua = req.header("User-agent")
+  if (ua !== undefined) {
+    const match = ua.match(/Gradle\/([\d\\.]+)/)
+    if (match !== null) {
+      return match[1]
+    }
+  }
+  // if(process.env.NODE_ENV !== "test") {
+  //   throw new InvalidParamsError(400, "Tried to query outside of gradle")
+  // }
+  return ""
+}
+
+
+//fileIds: 2724420-sources-2724421
+//descriptor: example-jei-238222
+//filename: example-jei-238222-2724420-classifier
+// =>
+//fileIdsSplit: ['2724420', 'sources', '2724421']
+//descriptorSplit: ['example', 'jei', '238222']
+//filenameSplit = ['example', 'jei', '238222', '2724420', 'classifier']
+const splitAndValidate = (fileIds: string, descriptor: string, filename: string): SplitData => {
+  const fileIdsSplit = fileIds.split("-")
+  const descriptorSplit = descriptor.split("-")
+  const filenameSplit = filename.split("-")
+  if (descriptorSplit.length < 2) {
+    throw new InvalidParamsError(400, "Descriptor now must be in the format `<modname>-<projectid>")
+  }
+
+  if (filenameSplit.length < 3) {
+    throw new InvalidParamsError(400, "Bad Request")
+  }
+  return { fileIdsSplit, descriptorSplit, filenameSplit }
+}
+
+//Joins all by the last item as `name,` and names the last item `id`
+//descriptorSplit: ['example', 'jei', '238222']
+// =>
+//name: 'example-jei'
+//id: '238222'
+const getNameAndIdFromDescriptor = (split: SplitData): DescriptorParts => {
+  const { descriptorSplit } = split
+  const name = descriptorSplit.slice(0, descriptorSplit.length - 1).join("-")
+  const id = descriptorSplit[descriptorSplit.length - 1]
+  return {
+    name, id
+  }
+
+}
+
+const createFilenameParts = (split: SplitData): FilenameParts => {
+  const { descriptorSplit, fileIdsSplit, filenameSplit } = split
+
+  //My brother in christ please rewrite this code. 
+  //Essentially, I'm using the previous URL artifacts to validate the URL, and get the classifier
+  const name = filenameSplit.slice(0, descriptorSplit.length - 1).join("-")
+  const id = filenameSplit[descriptorSplit.length - 1]
+  const file = filenameSplit.slice(descriptorSplit.length).slice(0, fileIdsSplit.length).join("-")
+  const classifier = filenameSplit.slice(descriptorSplit.length + fileIdsSplit.length).join("-")
+  //From Example
+  //    _name: 'example-jei'
+  //    _id: '238222'
+  //    _file: '2724420'
+  //    classifier: 'classifier'
+
+  return {
+    name, id, file, classifier
+  }
+}
+
+//Check the url parts match up
+const validatePartsEqual = (descriptorParts: DescriptorParts, filenameParts: FilenameParts, fileIds: string) => {
+  if (descriptorParts.name !== filenameParts.name || descriptorParts.id !== filenameParts.id || fileIds !== filenameParts.file) {
+    throw new InvalidParamsError(400, "Bad Request")
+  }
+}
+
+
+class InvalidParamsError extends Error {
+  constructor(
+    public statusCode: number,
+    public text: string,
+  ) {
+    super(`[${statusCode}]: ${text}`)
+  }
+}
+
+
+export default verifyParams
