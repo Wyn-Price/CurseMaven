d2d4fa51c70583134b3cdf835b125d31a365b5ce
Wyn Price <wynprice999@gmail.com>
2022-05-23 11:45:58 +0100

    Fetch bug

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,7 @@ export const authFetch = (url: string) => fetch(url, { headers: { 'x-api-key': p
 
 export const getDownloadUrl = (id: string, file: string | number) => `https://api.curseforge.com/v1/mods/${id}/files/${file}/download-url`
 export const fetchDownloadUrl = (id: string, file: string | number) => authFetch(getDownloadUrl(id, file))
+
 export const getFetchedData = async (response: Response) => {
   const json = await response.json()
   return json.data
