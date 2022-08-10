f78b4f1e9c8d076ad3b3496bc13628dc09fd6b1e
Wyn Price <wynprice999@gmail.com>
2022-08-10 17:00:39 +0100

    Fix issue with pom generation and classifiers

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ const app = express();
 const urlBase = "/curse/maven/:descriptor/:fileIds/:filename"
 
 app.get(`${urlBase}.jar`, verifyParams, normaljar)
+//Although we don't use `verifyParams` for the pom, it's still good to 
+//prevent malformed maven coordinates producing a valid pom
 app.get(`${urlBase}.pom`, verifyParams, pom)
 app.get(`${urlBase}.md5`, (_, res) => res.sendStatus(404))
 app.get(`${urlBase}.sha1`, (_, res) => res.sendStatus(404))
--- src/pom.ts
+++ src/pom.ts
@@ -1,15 +1,15 @@
 import { RequestHandler } from "express";
 
 const pom: RequestHandler = async (req, res) => {
-  const { name, id, file } = res.locals
+  const { descriptor, fileIds } = req.params
   return res.send(
     `<?xml version="1.0" encoding="UTF-8"?>
     <project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
       <modelVersion>4.0.0</modelVersion>
       <groupId>curse.maven</groupId>
-      <artifactId>${name}-${id}</artifactId>
-      <version>${file}</version>
+      <artifactId>${descriptor}</artifactId>
+      <version>${fileIds}</version>
     </project>`
   )
 }
