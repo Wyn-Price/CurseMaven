66ddb43aef0ae33f3fa21d9e0f9b5aa2887275cb
Wyn Price <github@wynprice.com>
2023-06-19 23:46:08 +0100

    Finish up moving everything to express

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,10 @@
-import fetch, { Response } from "node-fetch"
-
 export const authFetch = (url: string) => fetch(url, { headers: { 'x-api-key': process.env.API_KEY ?? "" } })
 
 export const getDownloadUrl = (id: string, file: string | number) => `https://api.curseforge.com/v1/mods/${id}/files/${file}/download-url`
 export const fetchDownloadUrl = (id: string, file: string | number) => authFetch(getDownloadUrl(id, file))
 
 export const getFetchedData = async (response: Response) => {
-  const json = await response.json()
+  const json = await response.json() as { data: string }
   return json.data
 }
 
