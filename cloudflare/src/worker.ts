94cc949e5811bc71616ab139ea77dca514b466bc
Wyn Price <github@wynprice.com>
2023-07-03 21:06:35 +0100

    Directly redirect cfa2 and www
    Closes #6

--- cloudflare/src/worker.ts
+++ cloudflare/src/worker.ts
@@ -1,13 +1,19 @@
+const hostRewrites: Record<string, string | undefined> = {
+    "www.cursemaven.com": "cursemaven.com",
+    "cfa2.cursemaven.com": "beta.cursemaven.com",
+}
+
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
         ctx.passThroughOnException()
 
-        // const PROXY_HOST_OVERRIDE = await env.KV_DATADOG.get("PROXY_HOST_OVERRIDE")
-        // if (PROXY_HOST_OVERRIDE !== null) {
-        //     const url = new URL(request.url);
-        //     url.host = PROXY_HOST_OVERRIDE;
-        //     request = new Request(url, request)
-        // }
+        // Rewrite the host, if it's using the old URLs (www & cfa2)
+        const hostOverride = hostRewrites[new URL(request.url).host];
+        if (hostOverride !== undefined) {
+            const url = new URL(request.url);
+            url.host = hostOverride;
+            request = new Request(url, request)
+        }
 
         const time = Date.now()
         const response = await fetch(request, { redirect: 'follow' })
@@ -31,6 +37,8 @@ export default {
             .filter(([_, value]) => value !== "")
             .forEach(([key, value]) => cursemavenData[key] = decodeURIComponent(value));
 
+        const url = new URL(request.url);
+
         // Weird structure is to match how vercel used to format data
         const metrics = {
             cursemaven: cursemavenData,
@@ -53,14 +61,14 @@ export default {
                 status_code: response.status,
             },
             proxy: {
-                path: new URL(request.url).pathname,
+                path: url.pathname,
                 statusCode: response.status,
             },
             statusCode: response.status,
             timestamp: Date.now(),
         };
 
-        const hostname = request.headers.get('host') ?? '';
+        const hostname = url.host;
 
         const data = {
             ddsource: 'cloudflare',
